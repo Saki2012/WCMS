a97f605fef117bcf6b3c1ca404130b6ae642890e
e914411-oss <e914411@gmail.com>
2025-09-03 13:33:35 +0800

    還原ErrorHelper的叫用

--- WCMS_Backend/SysCore/ApiDataController.cs
+++ WCMS_Backend/SysCore/ApiDataController.cs
@@ -22,8 +22,8 @@ namespace WCMS.SysCore
     {
         #region Property
         private IBizService<TSet>? _service;
-        protected IErrorHelper Message => _Message ??= HttpContext.RequestServices.GetRequiredService<ErrorHelper>();
-        private ErrorHelper? _Message;
+        protected IErrorHelper Message => _Message ??= HttpContext.RequestServices.GetRequiredService<IErrorHelper>();
+        private IErrorHelper? _Message;
         protected IBizService<TSet> Service => _service ??= HttpContext.RequestServices.GetRequiredService<IBizService<TSet>>();
         private IOutputCacheStore? _cacheStore;
         protected IOutputCacheStore CacheStore => _cacheStore ??= HttpContext.RequestServices.GetRequiredService<IOutputCacheStore>();
