e3106a16b0d9153311d47425f9819ffeedd7ff30
水滸 <sakitsubasa2012@gmail.com>
2025-11-17 11:31:23 +0800

    修正pdf在下載時提供預覽問題 (待驗)

--- WCMS_Backend/SysCore/SystemFunc/FileManagement/FileManagement_API.cs
+++ WCMS_Backend/SysCore/SystemFunc/FileManagement/FileManagement_API.cs
@@ -56,7 +56,7 @@ namespace WCMS.SysCore.SystemFunc.FileManagement
             else if (result.Count == 1) 
             {
                 string path = Path.Combine(Env.ContentRootPath,result[0].Path,$"{result[0].InternalId}.{result[0].FileExtension}");
-                if (result[0].FileExtension.Equals(FileExtensions.PDF)) return PhysicalFile(path, result[0].MimeType);
+                if (string.Equals(result[0].FileExtension, FileExtensions.PDF, StringComparison.OrdinalIgnoreCase)) return PhysicalFile(path, result[0].MimeType);
                 else 
                 return PhysicalFile(path, result[0].MimeType, fileDownloadName: $"{result[0].FileName}.{result[0].FileExtension}");
             }
