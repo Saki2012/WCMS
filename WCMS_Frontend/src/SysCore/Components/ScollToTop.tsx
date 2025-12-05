0cdd3617bd5ce9a9b4b638334c7f8824dd538d47
水滸 <sakitsubasa2012@gmail.com>
2025-12-05 10:42:11 +0800

    修正:同網址情況下也會回到最前面(例如首頁點logo時)

--- WCMS_Frontend/src/SysCore/Components/ScollToTop.tsx
+++ WCMS_Frontend/src/SysCore/Components/ScollToTop.tsx
@@ -3,13 +3,13 @@ import { useLocation } from "react-router-dom";
 
 // 每次路徑改變就捲到最上方
 export const ScrollToTop = () => {
-    const { pathname } = useLocation();
+    const { key } = useLocation();
     useEffect(() => {
         window.scrollTo({
             top: 0,
             left: 0,
             behavior: "smooth", // 不要動畫（可改 smooth）
         });
-    }, [pathname]);
+    }, [key]);
     return null;
 };
