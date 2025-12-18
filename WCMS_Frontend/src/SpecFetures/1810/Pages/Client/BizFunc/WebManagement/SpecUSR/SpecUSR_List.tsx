985a909aa1b76be1f55a91d0054497445018b3fd
水滸 <sakitsubasa2012@gmail.com>
2025-12-18 15:08:07 +0800

    熱修:USR計畫不限制筆數

--- WCMS_Frontend/src/SpecFetures/1810/Pages/Client/BizFunc/WebManagement/SpecUSR/SpecUSR_List.tsx
+++ WCMS_Frontend/src/SpecFetures/1810/Pages/Client/BizFunc/WebManagement/SpecUSR/SpecUSR_List.tsx
@@ -38,7 +38,7 @@ const useSpecUSRList = (categoryId: string, tagIds: string) => {
             [SpecUSRSetFields.SpecUSRDetail, SpecUSRDetailFields.Cohost2],
             [SpecUSRSetFields.SpecUSRDetail, SpecUSRDetailFields.Commissioned],
         ],
-        buildQueryCondition: (page) => ({
+        buildQueryCondition: () => ({
             Fields: [
                 SpecUSRModelFields.InternalId,
                 SpecUSRModelFields.USRId,
@@ -65,8 +65,8 @@ const useSpecUSRList = (categoryId: string, tagIds: string) => {
                 { Col: `${SpecUSRModelFields._SpecUSRDetail}.${SpecUSRDetailFields.Year}`, Desc: true },
                 { Col: `${SpecUSRModelFields._SpecUSRDetail}.${SpecUSRDetailFields.AcademicYear}`, Desc: true },
             ],
-            PageNumber: page,
-            PageSize: 10,
+            PageNumber: 0,
+            PageSize: 0,
         }),
         enabled: true,
         deps: [],
