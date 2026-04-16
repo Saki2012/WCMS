becfe1f87987cf83e61ea712c758d8a1dbf57c12
水滸 <sakitsubasa2012@gmail.com>
2026-04-16 09:57:37 +0800

    CSP追加

--- WCMS_Frontend/src/SSR/SSR-Server.ts
+++ WCMS_Frontend/src/SSR/SSR-Server.ts
@@ -709,7 +709,7 @@ const buildProdCsp = (nonce: string): string =>
         "font-src 'self' data: https://fonts.gstatic.com https://calendar.google.com",
         "connect-src 'self' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com https://calendar.google.com https://maps.googleapis.com https://mapsresources-pa.googleapis.com",
         "frame-ancestors 'self'",
-        "frame-src 'self' https://translate.google.com https://www.youtube.com https://www.youtube-nocookie.com https://w.soundcloud.com https://calendar.google.com https://www.google.com https://maps.google.com https://lookerstudio.google.com",
+        "frame-src 'self' https://translate.google.com https://www.youtube.com https://www.youtube-nocookie.com https://w.soundcloud.com https://calendar.google.com https://www.google.com https://maps.google.com https://lookerstudio.google.com https://datastudio.google.com",
         "object-src 'none'",
         "base-uri 'self'",
         "form-action 'self'",
