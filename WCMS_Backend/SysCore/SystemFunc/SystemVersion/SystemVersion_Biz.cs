d2146f2785fb13abb7e37cec18e43a5b040270e9
水滸 <sakitsubasa2012@gmail.com>
2026-07-21 14:22:17 +0800

    更新版號

--- WCMS_Backend/SysCore/SystemFunc/SystemVersion/SystemVersion_Biz.cs
+++ WCMS_Backend/SysCore/SystemFunc/SystemVersion/SystemVersion_Biz.cs
@@ -11,7 +11,7 @@ public class SystemVersion_Biz
         {
             FeatVersion = 0,
             ModelVersion = 0,
-            Patch = 6,
+            Patch = 7,
         };
         SetSpecVersion(data);
         string version = $@"1.{data.FeatVersion}.{data.ModelVersion}.{data.Patch}";
