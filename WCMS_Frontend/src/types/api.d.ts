df9f7814e1f2e56c0447de733f897d9e1ec419fa
水滸 <sakitsubasa2012@gmail.com>
2026-08-21 15:47:11 +0800

    更新Schema

--- WCMS_Frontend/src/types/api.d.ts
+++ WCMS_Frontend/src/types/api.d.ts
@@ -1179,6 +1179,42 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/Service/Auth/SessionDiagnostics": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get: {
+            parameters: {
+                query?: never;
+                header?: {
+                    /** @description i18n language (e.g. zh-TW / en) */
+                    "Accept-Language"?: string;
+                };
+                path?: never;
+                cookie?: never;
+            };
+            requestBody?: never;
+            responses: {
+                /** @description OK */
+                200: {
+                    headers: {
+                        [name: string]: unknown;
+                    };
+                    content?: never;
+                };
+            };
+        };
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
     "/Service/Banner/Create": {
         parameters: {
             query?: never;
